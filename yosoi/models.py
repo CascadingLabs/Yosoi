6f72a1d9978c9733877f436c0f3d018f62a539e8
AndrewPBerg <andberg9@gmail.com>
2026-02-01 19:25:14 -0500

    docs: added comments to models.py

--- yosoi/models.py
+++ yosoi/models.py
@@ -7,6 +7,16 @@ class FieldSelectors(BaseModel):
     tertiary: str = Field(description="Generic selector or 'NA'")
 
 
+# TODO Make this a dynamic class. Perhaps include a way for users to define via pydantic models?
+# >> Maybe we make a custom pydantic model that can be used to define what is being scraped?
+# >> This might look like YosoiContent(BaseModel):
+# >>     url: str FieldSelectors
+# >>     date: DATETIME FieldSelector
+# >>     body_text: str
+# >>     related_content: str
+# >> And then we can use this to generate the ScrapingConfig dynamically?
+
+
 class ScrapingConfig(BaseModel):
     headline: FieldSelectors
     author: FieldSelectors
